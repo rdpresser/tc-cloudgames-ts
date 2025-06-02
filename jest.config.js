06320980d36148090cbff588c2aac0a652b75ec4
Rodrigo Presser <rodrigo.presser@gmail.com>
2025-06-02 11:18:09 -0300

    feat: initialize TypeScript project with essential configurations and structure
    - Added package.json with scripts for build, lint, format, and testing.
    - Created initial TypeScript files for game and user use cases including handlers, requests, responses, and validators.
    - Implemented basic entity and value object structures for game and user aggregates.
    - Set up TypeScript configuration (tsconfig.json) with strict type checking and module resolution.
    - Added a sample unit test for a sum function.
    - Included a Dockerfile for containerization.

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+import { createDefaultPreset } from "ts-jest";
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+export const testEnvironment = "node";
+export const transform = {
+  ...tsJestTransformCfg,
+};
+export const testMatch = ['**/test/**/*.test.ts'];
